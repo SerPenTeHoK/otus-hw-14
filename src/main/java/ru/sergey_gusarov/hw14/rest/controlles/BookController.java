bb346dcbce07eec43c246ffbad8f8da9afc95e15
SerPenTeHoK <sergey-gusarov@yandex.ru>
2018-08-29 08:05:19 +0300

    add Author

--- src/main/java/ru/sergey_gusarov/hw14/rest/controlles/BookController.java
+++ src/main/java/ru/sergey_gusarov/hw14/rest/controlles/BookController.java
@@ -7,19 +7,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.sergey_gusarov.hw14.domain.books.Author;
 import ru.sergey_gusarov.hw14.domain.books.Book;
 import ru.sergey_gusarov.hw14.exception.NotFoundException;
+import ru.sergey_gusarov.hw14.service.books.AuthorService;
 import ru.sergey_gusarov.hw14.service.books.BookService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BookController {
     private final BookService bookService;
+    private final AuthorService authorService;
 
     @Autowired
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, AuthorService authorService) {
         this.bookService = bookService;
+        this.authorService = authorService;
     }
 
     @RequestMapping("/books")
@@ -53,5 +58,17 @@ public class BookController {
     public String deleteBook(@ModelAttribute Book book) {
         bookService.deleteById(book.getId());
         return "redirect:/books";
+    }
+
+    @RequestMapping(value = "/delAuthorFromBook")
+    public String deleteAuthorFromBook(@ModelAttribute Book book, @RequestParam("authorId") String authorId, Model model) {
+        Book bookFromDb = bookService.findById(book.getId()).orElseThrow(NotFoundException::new);
+        //Optional<Author> optionalAuthor = authorService.getById(authorId);
+        Author authorForDel = bookFromDb.getAuthors().stream()
+                .filter(p -> p.getId().equals(authorId))
+                .findFirst().get();
+        bookFromDb.getAuthors().remove(authorForDel);
+        bookService.save(bookFromDb);
+        return "redirect:/book?id="+bookFromDb.getId();
     }
 }
