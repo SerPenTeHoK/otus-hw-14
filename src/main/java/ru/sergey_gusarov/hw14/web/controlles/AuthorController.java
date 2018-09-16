83a48be4a61bcb64f47eb77cf69543047979e5ce
SerPenTeHoK <sergey-gusarov@yandex.ru>
2018-09-16 08:00:38 +0300

    edit by comments

--- src/main/java/ru/sergey_gusarov/hw14/web/controlles/AuthorController.java
+++ src/main/java/ru/sergey_gusarov/hw14/web/controlles/AuthorController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.sergey_gusarov.hw14.domain.books.Author;
 import ru.sergey_gusarov.hw14.domain.books.Book;
 import ru.sergey_gusarov.hw14.exception.NotFoundException;
@@ -30,7 +27,7 @@ public class AuthorController {
         this.bookService = bookService;
     }
 
-    @RequestMapping("/authors")
+    @GetMapping("/authors")
     public String listAuthorPage(Model model) {
         List<Author> authors = authorService.findAll();
         model.addAttribute("authors", authors);
